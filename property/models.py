db4ddf8f9cdfc71f8437b0b86c855247314731bf
Migheli <98918370+Migheli@users.noreply.github.com>
2022-05-03 11:28:56 +0300

    Created owners from flats objects

--- property/models.py
+++ property/models.py
@@ -85,12 +85,21 @@ class Claim(models.Model):
 
 
 class Owner(models.Model):
-    owner_name = models.CharField('ФИО владельца', max_length=200)
+    owner_name = models.CharField('ФИО владельца',
+                                  max_length=200,
+                                  db_index=True
+                                  )
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     owner_pure_phone = PhoneNumberField('Нормализованный номер владельца',
                                         null=True,
                                         blank=True,
+                                        db_index=True
                                         )
     flats_of_owner = models.ManyToManyField(Flat,
                                             verbose_name='Квартиры в собственности',
-                                            related_name='+')
+                                            related_name='owned_by',
+                                            db_index=True
+                                            )
+
+    def __str__(self):
+        return f'{self.owner_name}'
