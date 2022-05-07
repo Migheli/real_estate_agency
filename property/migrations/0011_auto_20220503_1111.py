381501bb5003444e7fc852458e19f3a4213915d7
Migheli <98918370+Migheli@users.noreply.github.com>
2022-05-07 05:36:43 +0300

    Owners creation migration modified
    iterator usage

--- property/migrations/0011_auto_20220503_1111.py
+++ property/migrations/0011_auto_20220503_1111.py
@@ -5,10 +5,10 @@ from django.db import migrations
 def owner_objects_autocreator(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     Owner = apps.get_model('property', 'Owner')
-    for flat in Flat.objects.all():
-        Owner.objects.get_or_create(owner_name=flat.owner,
-                                    owners_phonenumber=flat.owners_phonenumber,
-                                    owner_pure_phone=flat.owner_pure_phone)
+    for flat in Flat.objects.all().iterator():
+        Owner.objects.update_or_create(owner_name=flat.owner,
+                                       owners_phonenumber=flat.owners_phonenumber,
+                                       owner_pure_phone=flat.owner_pure_phone)
 
 
 class Migration(migrations.Migration):
